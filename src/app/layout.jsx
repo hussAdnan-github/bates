f671c571bf1cb2172620d65596ff306b67023a9b
hussain <hussbelf@gmail.com>
2026-06-30 08:31:56 -0700

    push

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -39,7 +39,6 @@ export default async function RootLayout({ children }) {
   return (
     <html lang="ar" dir="rtl">
       <body
-      
         style={{
           "--primary_color": primaryColor,
           "--secondary_color": secondaryColor,
